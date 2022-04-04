20df0549a86dae96ebb6a98ce3de42543f6c84dc
Leesungsup <tmdtjq0116@naver.com>
2022-04-04 23:58:49 +0900

    groupmessage

--- app/src/main/java/com/example/talk/chat/GroupMessageActivity.java
+++ app/src/main/java/com/example/talk/chat/GroupMessageActivity.java
@@ -6,12 +6,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.talk.R;
 import com.example.talk.model.ChatModel;
 import com.example.talk.model.UserModel;
@@ -25,19 +31,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class GroupMessageActivity extends AppCompatActivity {
     Map<String, UserModel> users = new HashMap<>();
     String destinationRoom;
     String uid;
     EditText editText;
-    private UserModel destinationUserModel;
     private DatabaseReference databaseReference;
     private ValueEventListener valueEventListener;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
     private RecyclerView recyclerView;
     List<ChatModel.Comment> comments = new ArrayList<>();
     @Override
@@ -51,9 +60,14 @@ public class GroupMessageActivity extends AppCompatActivity {
         FirebaseDatabase.getInstance().getReference().child("users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                users = (Map<String, UserModel>) dataSnapshot.getValue();
+                for(DataSnapshot item : dataSnapshot.getChildren()){
+                    users.put(item.getKey(),item.getValue(UserModel.class));
+                }
                 System.out.println(users.size());
                 init();
+                recyclerView = (RecyclerView) findViewById(R.id.groupMessageActivity_recyclerview);
+                recyclerView.setAdapter(new GroupMessageRecyclerViewAdapter());
+                recyclerView.setLayoutManager(new LinearLayoutManager(GroupMessageActivity.this));
             }
 
             @Override
@@ -61,9 +75,7 @@ public class GroupMessageActivity extends AppCompatActivity {
 
             }
         });
-        recyclerView = (RecyclerView) findViewById(R.id.groupMessageActivity_recyclerview);
-        recyclerView.setAdapter(new GroupMessageRecyclerViewAdapter());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
     void init(){
         Button button = (Button) findViewById(R.id.groupMessageActivity_button);
@@ -147,7 +159,41 @@ public class GroupMessageActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            GroupMessageViewHodler messageViewHolder = ((GroupMessageViewHodler) holder);
 
+
+            //내가보낸 메세지
+            if (comments.get(position).uid.equals(uid)) {
+                messageViewHolder.textView_message.setText(comments.get(position).message);
+                messageViewHolder.textView_message.setBackgroundResource(R.drawable.rightbubble_9);
+                messageViewHolder.linearLayout_destination.setVisibility(View.INVISIBLE);
+                messageViewHolder.textView_message.setTextSize(25);
+                messageViewHolder.linearLayout_main.setGravity(Gravity.RIGHT);
+                //setReadCounter(position, messageViewHolder.textView_readCounter_left);
+
+                //상대방이 보낸 메세지
+
+            } else {
+
+                Glide.with(holder.itemView.getContext())
+                        .load(users.get(comments.get(position).uid).profileImageUrl)
+                        .apply(new RequestOptions().circleCrop())
+                        .into(messageViewHolder.imageView_profile);
+                messageViewHolder.textview_name.setText(users.get(comments.get(position).uid).userName);
+                messageViewHolder.linearLayout_destination.setVisibility(View.VISIBLE);
+                messageViewHolder.textView_message.setBackgroundResource(R.drawable.leftbubble_9);
+                messageViewHolder.textView_message.setText(comments.get(position).message);
+                messageViewHolder.textView_message.setTextSize(25);
+                messageViewHolder.linearLayout_main.setGravity(Gravity.LEFT);
+                //setReadCounter(position, messageViewHolder.textView_readCounter_right);
+
+
+            }
+            long unixTime = (long) comments.get(position).timestamp;
+            Date date = new Date(unixTime);
+            simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+            String time = simpleDateFormat.format(date);
+            messageViewHolder.textView_timestamp.setText(time);
         }
 
         @Override
@@ -156,8 +202,25 @@ public class GroupMessageActivity extends AppCompatActivity {
         }
 
         private class GroupMessageViewHodler extends RecyclerView.ViewHolder {
+            public TextView textView_message;
+            public TextView textview_name;
+            public ImageView imageView_profile;
+            public LinearLayout linearLayout_destination;
+            public LinearLayout linearLayout_main;
+            public TextView textView_timestamp;
+            public TextView textView_readCounter_left;
+            public TextView textView_readCounter_right;
             public GroupMessageViewHodler(View view) {
                 super(view);
+                textView_message = (TextView) view.findViewById(R.id.messageItem_textView_message);
+                textview_name = (TextView)view.findViewById(R.id.messageItem_textview_name);
+                imageView_profile = (ImageView)view.findViewById(R.id.messageItem_imageview_profile);
+                linearLayout_destination = (LinearLayout)view.findViewById(R.id.messageItem_linearlayout_destination);
+                linearLayout_main = (LinearLayout)view.findViewById(R.id.messageItem_linearlayout_main);
+                textView_timestamp = (TextView)view.findViewById(R.id.messageItem_textview_timestamp);
+                textView_readCounter_left=(TextView) view.findViewById(R.id.messageItem_textview_readCounter_left);
+                textView_readCounter_right=(TextView) view.findViewById(R.id.messageItem_textview_readCounter_right);
+
             }
         }
     }
