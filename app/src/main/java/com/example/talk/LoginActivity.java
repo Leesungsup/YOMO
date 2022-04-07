cee032790cee75b9fc4bee96f65911a57c3764c3
Hyoung Jin <drj9802@gmail.com>
2022-04-07 23:42:58 +0900

    로그인 toast message Short -> Long

--- app/src/main/java/com/example/talk/LoginActivity.java
+++ app/src/main/java/com/example/talk/LoginActivity.java
@@ -82,7 +82,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (!task.isSuccessful()) {
-                            //로그인 실패한부분
+                            //로그인 실패한부분.
                             Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
                     }
