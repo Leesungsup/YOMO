5adbc84103be5343ae2957122abe585a3c3618dd
Hyoung Jin <drj9802@gmail.com>
2022-05-13 16:31:07 +0900

    people 화면에 filter 버튼 위치이동(왼쪽 하단) Filter Model 추가 (RecyclerView) SelectFilterActivity. RecyclerView 를 이용

---
+++ app/src/main/java/com/example/talk/model/FilterModel.java
@@ -0,0 +1,17 @@
+package com.example.talk.model;
+
+public class FilterModel {
+    public String menu;
+
+    public FilterModel(String menu){
+        this.menu= menu;
+    }
+
+    public String getMenu(){
+        return menu;
+    }
+
+    public void setMenu(String menu){
+        this.menu = menu;
+    }
+}
